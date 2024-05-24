70e16d9ed6d1dfdb0b8c72df1f6af2fcc3b6c254
anichka21 <anashnaidruk@gmail.com>
2024-05-24 17:58:15 +0300

    crud

--- src/route/index.js
+++ src/route/index.js
@@ -34,7 +34,7 @@ router.get('/', function (req, res) {
 // router.get Створює нам один ентпоїнт
 
 // ↙️ тут вводимо шлях (PATH) до сторінки
-router.get('/', function (req, res) {
+router.get('/purchase-index', function (req, res) {
   // res.render генерує нам HTML сторінку
 
   // ↙️ cюди вводимо назву файлу з сontainer
